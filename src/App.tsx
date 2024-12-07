61c87a8f4b844a4f52c0991985fb7220b23563e3
Hendrik Käo <hendrik.kao@gmail.com>
2024-12-07 17:23:27 +0100

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,52 @@
+import { useEffect } from 'react';
 import './App.css'
+import { createAnthropic } from '@ai-sdk/anthropic';
+import { generateText } from 'ai';
+
+const anthropic = createAnthropic({
+  apiKey: '',
+  baseURL: '/llm',
+  fetch(input, init) {
+    const headers = new Headers(Object.entries(init?.headers ?? {}));
+    headers.append('anthropic-dangerous-direct-browser-access', 'true');
+
+    return fetch(input, {
+      ...init,
+      headers,
+    });
+  },
+});
+
+
+const getText = async () => {
+  const { text } = await generateText({
+    model: anthropic('claude-3-5-haiku-latest', {
+      cacheControl: true,
+    }),
+    messages: [
+      {
+        role: 'system',
+        content: 'Nexthink expert',
+        experimental_providerMetadata: {
+          anthropic: { cacheControl: { type: 'ephemeral' } },
+        }
+      },
+      {
+        role: 'user',
+        content: 'Write a vegetarian lasagna recipe for 4 people.',
+      }
+    ],
+  });
+
+  return text;
+}
 
 function App() {
+
+  useEffect(() => {
+    // getText().then(console.log)
+  }, [])
+
   return (
     <h1>Gen AI Dashboards</h1>
   )
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/llm": {
+        target: "https://api.anthropic.com/v1",
+        changeOrigin: true,
+        rewrite: (path) => {
+          const newPath = path.replace(/^\/llm/, "");
+          return newPath;
+        },
+      },
+    },
+  },
+});
